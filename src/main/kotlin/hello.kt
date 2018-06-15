63e97c5767e308a12cf2f0633938ad11f4c91b3c
Carlos Roberto Rojas <carlosroberto.rojas@gmail.com>
2018-06-15 12:45:18 -0500

    consulta de ususarios con parámetros de páginación

--- src/main/kotlin/hello.kt
+++ src/main/kotlin/hello.kt
@@ -1,10 +1,17 @@
 import spark.kotlin.*
 
 fun main(args: Array<String>) {
-    val http: Http = ignite()
+//    val http: Http = ignite()
 
-    http.get("/hello") {
+    get("/listarUsuarios/:pagina/:numelem") {
+        val pagina = params(":pagina").toInt()        
+        val numelem = params(":numelem").toInt()  
         response.type("application/json")
-        App.getMyData()
+        App.listarUsuarios(pagina, numelem)
+    }
+
+    get("/listarUsuarios") {
+        response.type("application/json")
+        App.listarUsuarios(1, 10)
     }
 }
